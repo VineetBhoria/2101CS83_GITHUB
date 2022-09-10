93c6800b79d33fa85e50c7d17617e626f50093c7
vineetbhoria <vineetbhoria9@gmail.com>
2022-09-10 23:23:42 +0530

    int function added and main function

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the first number ";
+    cin>>a;
+    cout<<endl<<"Enter the second number ";
+    cin>>b;
+    cout<<endl;
+    return 0;
+}
