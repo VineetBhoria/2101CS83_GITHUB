29fa202ab588ccfa9bca074724d1f5c5e01c6583
vineetbhoria <vineetbhoria9@gmail.com>
2022-09-10 23:38:22 +0530

    changes done in the main function and added done

--- calculator.cpp
+++ calculator.cpp
@@ -1,61 +1,65 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int add(int a, int b)
+int add(int a, int b) // addition fn
 {
-    return a+b;
+    return a + b;
 }
 
-int sub(int a, int b)
+int sub(int a, int b) // subtration fn
 {
-    return a-b;
+    return a - b;
 }
 
-int mult(int a, int b)
+int mult(int a, int b) // multiply fn
 {
-    return a*b;
+    return a * b;
 }
 
-int division(int a, int b)
+int division(int a, int b) // division fn
 {
-    return a/b;
+    return a / b;
 }
 
+// this is the main function
 int main()
 {
-    int a,b,c,i;
-    cout<<"Enter the first number ";
-    cin>>a;
-    cout<<endl<<"Enter the second number ";
-    cin>>b;
-    cout<<endl;
-    cout<<"Press 1 for add "<<endl;
-    cout<<"Press 2 for sub "<<endl;
-    cout<<"Press 3 for mult "<<endl;
-    cout<<"Press 4 for div "<<endl;
-    cin>>c;
-    cout<<endl;
+    int a, b, c, i; // var declaration
+
+    cout << "Press 1 for add " << endl;
+    cout << "Press 2 for sub " << endl;
+    cout << "Press 3 for mult " << endl;
+    cout << "Press 4 for div " << endl;
+    cin >> c;
+    cout << endl;
+
+    cout << "Enter the first number ";
+    cin >> a;
+    cout << endl
+         << "Enter the second number ";
+    cin >> b;
+    cout << endl;
     switch (c)
     {
     case 1:
-        i=add(a,b);
-        cout<<"The result is "<<i;
+        i = add(a, b);
+        cout << "The result is " << i;
         break;
     case 2:
-        i=sub(a,b);
-        cout<<"The result is "<<i;
+        i = sub(a, b);
+        cout << "The result is " << i;
         break;
     case 3:
-        i=mult(a,b);
-        cout<<"The result is "<<i;
+        i = mult(a, b);
+        cout << "The result is " << i;
         break;
     case 4:
-        i=division(a,b);
-        cout<<"The result is "<<i;
+        i = division(a, b);
+        cout << "The result is " << i;
         break;
-    
+
     default:
-    cout<<"Wrong choice ";
+        cout << "Wrong choice ";
         break;
     }
 
