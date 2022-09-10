7f6e4e06e9e4ddbeae097ec10ba1576560fe9c99
vineetbhoria <vineetbhoria9@gmail.com>
2022-09-10 17:01:51 +0530

    merge sort code written with one function merge left and commited

---
+++ merge_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Merge two subarrays L and M into arr
+
+
+// Divide the array into two subarrays, sort them and merge them
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // m is the point where the array is divided into two subarrays
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted subarrays
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {6, 5, 12, 10, 9, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted array: \n";
+    printArray(arr, size);
+    return 0;
+}
